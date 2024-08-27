f15265f1820750c1157f49eaf50b84099d60737f
zavvar-sayid <zavvar.sayid@gmail.com>
2024-08-27 23:32:04 +0330

    6-4

---
+++ Models/VM/FilterVM.cs
@@ -0,0 +1,15 @@
+﻿namespace G_Wallet_API.Models.VM;
+
+public class FilterVM
+{
+
+    public int UserId { get; set; }
+    public DateTime? ToDate { get; set; }
+    public DateTime? FromDate { get; set; }
+    public long? WalletId { get; set; }
+    public long? WalletCurrencyId { get; set; }
+    public short? TransactionTypeId { get; set; }
+    public short? TransactionModeId { get; set; }
+    public short? CurrencyId { get; set; }
+
+}
